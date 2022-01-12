4fc80a2729da8ff3f31b4fee30f95b6dbe796a9b
Lucas Lima <lucas.lima-95@hotmail.com>
2022-01-12 01:04:05 -0300

    Revert "block 22.1. Exercises completed."
    This reverts commit dbf00f09a19257ada02b20ad7174a42f03bbe905.

--- backend/block22/22.1/index.js
+++
@@ -1,9 +0,0 @@
-const readline = require('readline-sync');
-
-const options = ['imc', 'velocidade', 'sorteio', 'fatorial', 'fibonacci'];
-
-const escolha = readline.keyInSelect(options, 'Qual script deve ser executado? ');
-
-console.log(`Escolhido ${options[escolha]}.`)
-
-require(`./${options[escolha]}`)
