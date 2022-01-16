70c8f45136c6d7ccc866c9923351a49b3763c3d1
Lucas Lima <lucas.lima-95@hotmail.com>
2022-01-16 07:11:26 -0300

    Block 22.5. Exercises UNcompleted.

--- backend/block22/22.5/3_exercise.js
+++ backend/block22/22.5/3_exercise.js
@@ -1,12 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const { notFound } = require('./3_middlewares');
+const rescue = require('express-rescue');
+
 
 const app = express();
 app.use(bodyParser.json());
 
-app.get('/posts/:id', (req, res) => {
-  const { id } = req.params;
-})
+const routes = require('./3_routes');
+
+app.use('/posts', routes);
+
+app.all('*', rescue(notFound));
 
 app.listen(3000, () => {
   console.log('bom dia!')
---
+++ backend/block22/22.5/3_posts.js
@@ -0,0 +1,9 @@
+const posts = [
+  { id: 1, title: "JavaScript tips and tricks", year: 2020 }, 
+  { id: 2, title: "JavaScript is not cool", year: 2020 }, 
+  { id: 3, title: "JavaScript is AWESOME", year: 2021 }, 
+  { id: 4, title: "Crazy JavaScript stuff", year: 2022 }, 
+]
+module.exports = {
+  posts,
+}
