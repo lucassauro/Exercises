70de965c16d313cb4a0dd27f3b4a5e529a368c37
Lucas Lima <lucas.lima-95@hotmail.com>
2022-01-20 11:51:43 -0300

    Block 23.1. Exercises Completed.

--- backend/block23/23.1/model-example/index.js
+++ backend/block23/23.1/model-example/index.js
@@ -1,43 +1,31 @@
 const express = require('express');
-const BookModels = require('./models/BookModels');
-const { validateInfo } = require('./middlewares/middlewares');
+const { validateInfo, validateUser, validateUserId } = require('./middlewares/validationMiddlewares');
+const { getBookByAuthorId, getAllBooks, getBookById, createBook, deleteBookById, createUser, getAllUsers, getUserById, updateUser } = require('./middlewares/middlewares')
 
 require('dotenv').config()
 
 const app = express();
 app.use(express.json());
 
-app.get('/book', async (req, res) => {
-  const { author_id } = req.query;
-  const book = await BookModels.getByAuthorId(author_id)
-  if (!book) return res.status(404).json({ message: "Not found"});
-  res.status(200).json(book);
-})
+app.get('/book', getBookByAuthorId)
 
-app.get('/books', async (req, res) => {
-  const books = await BookModels.getAll();
-  if (!books) return res.status(404)
-  res.status(200).json(books);
-})
+app.get('/books', getAllBooks)
 
-app.get('/books/:id', async (req, res) => {
-  const { id } = req.params;
-  const books = await BookModels.getBookById(id);
-  if (!books || books.length < 1) return res.status(404).json({ message: "Not found"});
-  res.status(200).json(books);
-})
+app.get('/books/:id', getBookById)
 
-app.post('/books', validateInfo, async (req, res) => {
-  const { title, author_id} = req.body;
-  const create = await BookModels.createBook(title, author_id);
-  res.status(201).json({ message: `Livro criado com sucesso! Id número ${create}` })
-})
+app.post('/books', validateInfo, createBook)
 
-app.delete('/books/:id', async (req, res) => {
-  const { id } = req.params;
-  await BookModels.deleteById(id);
-  res.status(204).json({ message: `Arquivo ${id} deletado com sucesso.` })
-} )
+// não consta nos exercícios, endpoint criado para estudar insertId retornado da função connection.execute();
+app.delete('/books/:id', deleteBookById)
+
+
+app.get('/user', getAllUsers) 
+
+app.post('/user', validateUser, createUser)
+
+app.get('/user/:id', getUserById)
+
+app.put('/user/:id', validateUser, validateUserId, updateUser)
 
 const PORT = process.env.PORT || 3000;
 
--- backend/block23/23.1/model-example/middlewares/middlewares.js
+++ backend/block23/23.1/model-example/middlewares/middlewares.js
@@ -1,15 +1,72 @@
-const { getByAuthorId } = require('../models/BookModels');
+const BookModels = require('../models/BookModels');
+const UserModels = require('../models/UserModels');
 
+// Middlewares Books
+const getBookByAuthorId = async (req, res) => {
+  const { author_id } = req.query;
+  const book = await BookModels.getByAuthorId(author_id)
+  if (!book) return res.status(404).json({ message: "Not found"});
+  res.status(200).json(book);
+};
 
-const validateInfo = async (req, res, next) => {
-  const { title, author_id } = req.body;
-  const test = await getByAuthorId(author_id);
-  if (!title || !author_id || title.length < 3) return res.status(400).json({ message: "Dados inválidos" })
-  if (test.length < 1) return res.status(400).json({ message: "Dados inválidos" });
+const getAllBooks = async (_req, res) => {
+  const books = await BookModels.getAll();
+  if (!books) return res.status(404)
+  res.status(200).json(books);
+}
 
-  next();
+const getBookById = async (req, res) => {
+  const { id } = req.params;
+  const books = await BookModels.getBookById(id);
+  if (!books || books.length < 1) return res.status(404).json({ message: "Not found"});
+  res.status(200).json(books);
+}
+
+const createBook = async (req, res) => {
+  const { title, author_id} = req.body;
+  const create = await BookModels.createBook(title, author_id);
+  res.status(201).json({ message: `Livro criado com sucesso! Id número ${create}` })
+}
+
+const deleteBookById = async (req, res) => {
+  const { id } = req.params;
+  await BookModels.deleteById(id);
+  res.status(204).json({ message: `Arquivo ${id} deletado com sucesso.` })
+} 
+
+const getAllUsers = async (_req, res) => {
+  const getAll = await UserModels.getUsers();
+  res.status(200).json(getAll);
+}
+
+const getUserById = async (req, res) => {
+  const { id } = req.params;
+  const getAll = await UserModels.getUsers(id);
+  if (!getAll || getAll.length < 1) return res.status(404).json({ error: true, message: "Usuário não encontrado." })
+  res.status(200).json(getAll);
+}
+
+const createUser = async (req, res) => {
+  const user = req.body;
+  const create = await UserModels.create(user);
+  res.status(201).json(create);
+}
+
+const updateUser = async (req, res) => {
+  const { id } = req.params;
+  const body = req.body;
+  const update = await UserModels.update(id, body);
+  res.status(200).json(update);
 }
 
 module.exports = {
-  validateInfo,
+  getBookByAuthorId,
+  getAllBooks,
+  getBookById,
+  createBook,
+  deleteBookById,
+  getAllUsers,
+  createUser,
+  getUserById,
+  updateUser,
 }
